38c30f43f150ca6c48196452247bb88d24eb5429
Sumith Damodaran <sumithpd@gmail.com>
2024-01-18 00:42:13 +0100

    refactor to smaller widget for film cell

--- supa_movies_app/lib/pages/home_page.dart
+++ supa_movies_app/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:supa_movies_app/models/dummy_data.dart';
 
+import '../widgets/film_cell.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -8,55 +10,16 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var films = dummydata;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Films'),
-        ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            final film = films[index];
-            return InkWell(
-              onTap: () {},
-              child: Stack(
-                children: [
-                  AspectRatio(
-                    aspectRatio: 16 / 9,
-                    child: HeroMode(
-                      enabled: true,
-                      child: Hero(
-                        tag: film.imageUrl,
-                        child: Image.network(film.imageUrl),
-                      ),
-                    ),
-                  ),
-                  Positioned.fill(
-                    top: null,
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        colors: [
-                          Colors.black,
-                          Colors.black.withAlpha(0),
-                        ],
-                      )),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          film.title,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-          itemCount: films.length,
-        ));
+      appBar: AppBar(
+        title: const Text('Films'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final film = films[index];
+          return FilmCell(film: film);
+        },
+        itemCount: films.length,
+      ),
+    );
   }
 }
