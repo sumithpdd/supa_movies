5c5e374265348fe8cb2304c6f2d18c5a129365fe
Sumith Damodaran <sumithpd@gmail.com>
2024-01-18 00:36:22 +0100

    Showing movies list

--- supa_movies_app/lib/pages/home_page.dart
+++ supa_movies_app/lib/pages/home_page.dart
@@ -11,8 +11,52 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Films'),
         ),
-        body: Center(
-          child: Text("Movies -   ${films.length}"),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            final film = films[index];
+            return InkWell(
+              onTap: () {},
+              child: Stack(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: HeroMode(
+                      enabled: true,
+                      child: Hero(
+                        tag: film.imageUrl,
+                        child: Image.network(film.imageUrl),
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    top: null,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                          Colors.black,
+                          Colors.black.withAlpha(0),
+                        ],
+                      )),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          film.title,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          itemCount: films.length,
         ));
   }
 }
