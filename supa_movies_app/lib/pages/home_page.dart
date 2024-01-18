042e0b84c9959fa7613f5ad0588a53b61498e8e6
Sumith Damodaran <sumithpd@gmail.com>
2024-01-18 01:27:24 +0100

    Homepage list from supabase

--- supa_movies_app/lib/pages/home_page.dart
+++ supa_movies_app/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:supa_movies_app/models/dummy_data.dart';
 
+import '../data/initialise_supabase.dart';
+import '../models/film.dart';
 import '../widgets/film_cell.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,18 +10,35 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var films = dummydata;
+    var filmsFuture = supabase
+        .from('films')
+        .select()
+        .withConverter<List<Film>>((data) => data.map(Film.fromJson).toList());
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Films'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final film = films[index];
-          return FilmCell(film: film);
-        },
-        itemCount: films.length,
-      ),
+      body: FutureBuilder(
+          future: filmsFuture,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final films = snapshot.data!;
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                final film = films[index];
+                return FilmCell(film: film);
+              },
+              itemCount: films.length,
+            );
+          }),
     );
   }
 }
